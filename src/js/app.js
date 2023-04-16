cd49a78b8a2af2736ff2bdb98a2db92416cc15fd
Margarita May Lee <ritochka.m_lee@yahoo.com>
2023-04-16 18:59:00 +0300

    First Commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
+import image from '../img/goblin.png';
 
-
-export default class goblinGame {
+export default class GoblinGame {
   constructor() {
-    this.position = 1;
+    this.position = -1;
     this.field = 0;
     this.boarSize = 1;
+    this.successShot = 0;
+    this.bossShot = 0;
+    this.rounds = 0;
+    this.gameResults = document.createElement('p');
   }
 
   goblinSetter() {
@@ -28,14 +32,43 @@ export default class goblinGame {
       cell.classList.add('cell');
       this.field.appendChild(cell);
     }
+    document.body.appendChild(this.gameResults);
   }
 
   imgCreate(interval) {
-    const goblin = document.createElement('img');
-    goblin.setAttribute('src', '../img/goblin.png');
-    setInterval(() => {
+    const goblin = new Image();
+    goblin.src = image;
+    goblin.classList.add('red-head');
+    let intervalId = setInterval(() => {
+      if (this.bossShot > 4) {
+        clearInterval(intervalId);
+        intervalId = null;
+        document.body.insertAdjacentHTML(
+          'beforeEnd',
+          `<div class="modal_mask">
+            <div class="modal">
+              <div class="modal_msg">Ты был не точен, игра окончена.</div>
+              <button class="close_btn">Закрыть</button>
+            </div>
+          </div>`,
+        );
+        const clsModalBtn = document.querySelector('.close_btn');
+        clsModalBtn.addEventListener('click', (ev) => ev.target.closest('div.modal_mask').remove());
+      }
       const position = this.goblinSetter();
       this.field.childNodes[position].appendChild(goblin);
+      this.rounds += 1;
+      this.gameResults.textContent = `Попадания: ${this.successShot} / Промахи: ${this.bossShot} / Число попыток: ${this.rounds}`;
     }, interval);
+  }
+
+  shootsRecorder() {
+    document.body.addEventListener('click', (ev) => {
+      if (ev.target.classList.contains('red-head')) {
+        this.successShot += 1;
+      } else if (ev.target.classList.contains('cell')) {
+        this.bossShot += 1;
+      }
+    });
   }
 }
